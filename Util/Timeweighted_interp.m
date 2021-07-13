6c52238c547f820e1d864873bea289d95377ef6a
bbncWLG <51014979+bbncWLG@users.noreply.github.com>
2021-07-13 17:26:47 +0800

    Update Timeweighted_interp.m

--- Util/Timeweighted_interp.m
+++ Util/Timeweighted_interp.m
@@ -8,9 +8,9 @@ function znew = Timeweighted_interp(ref,index,a1,a2,Nshift)
 %% Output:
 % new_im: the shifted 3D image  
 %
-%    [1]  JIAMIN WU, ZHI LU, DONG JIANG and YUDUO GUO.etc,
-%         3D observation of large-scale subcellular dynamics in vivo at the millisecond scale
-%         in BioRxiv, 2019. 
+%   [1]  JIAMIN WU, ZHI LU and DONG JIANG etc,
+%        Iterative tomography with digital adaptive optics permits hour-long intravital observation of 3D subcellular dynamics at millisecond scale
+%        Cell, 2021. 
 %
 %    Contact: ZHI LU (luz18@mails.tsinghua.edu.cn)
 %    Date  : 10/24/2020
